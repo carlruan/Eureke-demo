bde405fe19db2782075e00f94d075f0b4fabee6c
carlruan <62496966+carlruan@users.noreply.github.com>
2022-08-17 20:26:42 -0400

    Initial commit

---
+++ src/main/java/com/kaifeng/eurekaclienta/EurekaClientAApplication.java
@@ -0,0 +1,13 @@
+package com.kaifeng.eurekaclienta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClientAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaifeng/eurekaclienta/EurekaClientAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaifeng.eurekaclienta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
